a2f07a940f825bf38f2681915cfb4f56fc5297af
Lindsey White <LindseyWhite053@Gmail.com>
2022-05-07 16:11:46 -0400

    for review

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,3 +2,34 @@ var dateDisplayEl = document.querySelector("#currentDay");
 
 dateDisplayEl.textContent = moment().format("dddd MMM Do");  
 
+var currentDate = new Date();
+var hours = currentDate.getHours();
+
+
+console.log(currentDate); 
+console.log(hours); 
+
+
+
+// //assign a time value to each time block 
+// var baseTime = new Date();
+// var nineAmEl = baseTime.setHours(9);
+var nineAm = moment().set("hour", 9);
+console.timeLog(nineAm);
+//evaluate the time to be past present or future 
+
+//if in the past add class .past 
+var timeEvaluation = function() {
+    if (hours > 9){
+        $("#nineAm").addClass("past")
+    }
+    // else if (hours > 10) {
+        
+    // }
+}
+
+//if in the present add class .present
+
+//if in the future add class.future
+
+
